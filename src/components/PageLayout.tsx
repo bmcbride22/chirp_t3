88309d8d2b81c6505f694da2d025aa88cce0dfcf
Brendan McBride <brendan.mcmath@gmail.com>
2023-03-25 16:59:32 +0800

    fix: remove overflow scroll bar

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -3,7 +3,7 @@ import { type PropsWithChildren } from 'react'
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className='flex justify-center'>
-      <div className=' h-full min-h-screen w-full overflow-y-scroll border-x border-slate-400 md:max-w-3xl'>
+      <div className=' overflow-none h-full min-h-screen w-full border-x border-slate-400 md:max-w-3xl'>
         {props.children}
       </div>
     </main>
