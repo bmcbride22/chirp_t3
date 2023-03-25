9794a64b34b863a5e4c80c921189d45245f2249e
Brendan McBride <brendan.mcmath@gmail.com>
2023-03-25 15:10:30 +0800

    feat: set up route for profil page, fetch profile using slug

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,16 +1,14 @@
 import clerkClient from "@clerk/clerk-sdk-node";
-import type {User} from "@clerk/nextjs/dist/api"
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
-const filterUserForClient = (user: User) => {
-	return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
-}
+
 
 
 // Create a new ratelimiter, that allows 3 requests per 60 seconds
